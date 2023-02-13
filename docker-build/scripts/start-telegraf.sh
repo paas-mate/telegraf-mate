3b6633f6ec4c987d7860978c1ae13154e935d9e5
goflutterjava <goflutterjava@gmail.com>
2023-02-13 17:36:05 +0800

    auto gen telegraf config

--- docker-build/scripts/start-telegraf.sh
+++ docker-build/scripts/start-telegraf.sh
@@ -2,4 +2,8 @@
 
 mkdir -p $TELEGRAF_HOME/logs
 CONF_FILE=${TELEGRAF_HOME}/conf/telegraf.conf
+echo "[agent]" >$CONF_FILE
+echo '  interval = "10s"' >>$CONF_FILE
+echo '  round_interval = true' >>$CONF_FILE
+echo "[[inputs.cpu]]" >>$CONF_FILE
 nohup $TELEGRAF_HOME/usr/bin/telegraf --config ${CONF_FILE} >>$TELEGRAF_HOME/logs/telegraf.stdout.log 2>>$TELEGRAF_HOME/logs/telegraf.stderr.log &
